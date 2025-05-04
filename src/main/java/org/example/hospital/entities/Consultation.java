ecfad424143cf6c4bca503ce5b2721dd96752d6e
wiameanejjar <wiameanejjar@gmail.com>
2025-05-04 20:54:32 +0100

    1ér commit

---
+++ src/main/java/org/example/hospital/entities/Consultation.java
@@ -0,0 +1,19 @@
+package org.example.hospital.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Consultation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date dateConsulation;
+    private String rapport;
+    @OneToOne
+    private RendezVous rendezVous;
+
+}
---
+++ src/main/java/org/example/hospital/entities/StatusRDV.java
@@ -0,0 +1,8 @@
+package org.example.hospital.entities;
+
+public enum StatusRDV {
+    PENDING,
+    CANCELLED,
+    DONE
+
+}
